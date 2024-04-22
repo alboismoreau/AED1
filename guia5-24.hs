74fdbf0316af89bafb58fe4f046f0f3b183b4776
Ana Boismoreau <143358214+alboismoreau@users.noreply.github.com>
2024-04-22 13:26:43 -0300

    Add files via upload

--- guia5-24.hs
+++ guia5-24.hs
@@ -57,5 +57,85 @@ eliminarRepetidos [x] = [x]
 eliminarRepetidos (x:xs) | pertenece x xs = eliminarRepetidos xs
                          | otherwise = eliminarRepetidos [x] ++ eliminarRepetidos xs
 
---mismosElementos :: (Eq t) => [t] -> [t] -> Bool
---mismosElementos l s 
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos [] [] = True
+mismosElementos [] [x] = True
+mismosElementos [x] l = pertenece x l 
+mismosElementos l s | pertenece (head(eliminarRepetidos s)) l = mismosElementos (tail s) l && mismosElementos s l
+                    | otherwise = False -- ARREGLAR
+
+capicua :: (Eq t) => [t] -> Bool
+capicua l = reverso l == l
+
+-- EJERCICIO 3
+
+sumatoria :: [Int] -> Int
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
+
+productoria :: [Int] -> Int
+productoria [x] = x 
+productoria [] = 0
+productoria (x:xs) = x * productoria xs 
+
+maximo :: [Int] -> Int
+maximo [x] = x
+maximo (x:y:xs) | x >= y = maximo (x:xs)  
+                | otherwise = maximo (y:xs)
+
+sumarN :: Int -> [Int] -> [Int]
+sumarN _ [] = []
+sumarN n [x] = [x+n]
+sumarN n (x:xs) = [x+n] ++ sumarN n xs 
+
+sumarElPrimero :: [Int] -> [Int]
+sumarElPrimero l = sumarN (head l) l
+
+sumarElUltimo :: [Int] -> [Int]
+sumarElUltimo l = sumarN (ultimo l) l
+
+pares :: [Int] ->  [Int] -- Deja solo pares en el mismo orden
+pares [] = []
+pares (x:xs) | esPar x = [x] ++ pares xs 
+             | otherwise = pares xs
+             where esPar :: Int -> Bool
+                   esPar n = mod n 2 == 0
+    
+multiplosDeN :: Int ->  [Int] ->  [Int] -- Deja solo multiplos de n en el mismo orden
+multiplosDeN _ [] = []
+multiplosDeN n (x:xs) | esMultiplo x n = [x] ++ multiplosDeN n xs 
+                      | otherwise = multiplosDeN n xs
+                      where esMultiplo :: Int -> Int -> Bool
+                            esMultiplo a b = mod a b == 0
+
+ordenar :: [Int] -> [Int] -- ordena en forma creciente
+ordenar [] = []
+ordenar (x:xs) | x == maximo (x:xs) = ordenar xs ++ [x]
+               | otherwise = ordenar (xs ++ [x]) 
+
+-- EJERCICIO 4
+
+sacarBlancosRepetidos :: [Char] -> [Char]
+sacarBlancosRepetidos [] = []
+sacarBlancosRepetidos (x:y:xs) | x == y && x == ' ' = sacarBlancosRepetidos (y:xs)
+                               | otherwise = x : (sacarBlancosRepetidos (y:xs))
+
+eliminaBlancosPrincipio ::  [Char] -> [Char]
+eliminaBlancosPrincipio [] = []
+eliminaBlancosPrincipio l | head(l) == ' ' = tail l
+                          | otherwise = l 
+
+eliminaBlancosIniFinal ::  [Char] -> [Char]
+eliminaBlancosIniFinal [] = []
+eliminaBlancosIniFinal l | ultimo(l) == ' ' = principio (eliminaBlancosPrincipio l)
+                      | otherwise = eliminaBlancosPrincipio l
+
+contarBlancos :: [Char] -> Int
+contarBlancos [] = 0
+contarBlancos (x:xs) | x == ' ' = 1 + contarBlancos xs 
+                     | otherwise = contarBlancos xs
+
+contarPalabras :: [Char] -> Int
+contarPalabras l = (contarBlancos l) + 1
+
+palabras :: [Char] -> [[Char]]
