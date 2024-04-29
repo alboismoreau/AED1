582ea05b0479c8859490a8c001b4b6453d8caf53
Ana Boismoreau <143358214+alboismoreau@users.noreply.github.com>
2024-04-29 13:25:14 -0300

    Add files via upload

--- simulacrotest.hs
+++ simulacrotest.hs
@@ -2,11 +2,11 @@ import Simulacro
 import Test.HUnit
 
 masAmigos = test [
-    "una sola tupla" ~: (personaConMasAmigos [("ana","luz")]) ~?= "ana" || "luz"
-    "empate con repetidos" ~: (personaConMasAmigos [("ana","luz"),("luz","marcos"),("marcos","maria"),("rita","mel")]) ~?= "luz" || "marcos"
-    "empate sin repetidos" ~: (personaConMasAmigos [("ana","luz"),("pato","marcos")]) ~?= "luz" || "marcos" || "pato" || "ana"
-    "gana primera posicion" ~: (personaConMasAmigos [("ana","luz"),("ana","marcos"),("marcos","maria"),("rita","mel")]) ~?= "ana"
-    "gana segunda posicion" ~: (personaConMasAmigos [("ana","luz"),("pato","ma"),("marcos","m"),("rita","luz")]) ~?= "luz" || "luz"
+    "una sola tupla" ~: (personaConMasAmigos [("ana","luz")]) ~?= "ana", -- || "luz"
+    "empate con repetidos" ~: (personaConMasAmigos [("ana","luz"),("luz","marcos"),("marcos","maria"),("rita","mel")]) ~?= "luz", 
+    "empate sin repetidos" ~: (personaConMasAmigos [("ana","luz"),("pato","marcos")]) ~?= "luz", -- || "marcos" || "pato" || "ana"
+    "gana primera posicion" ~: (personaConMasAmigos [("ana","luz"),("ana","marcos"),("marcos","maria"),("rita","mel")]) ~?= "ana",
+    "gana segunda posicion" ~: (personaConMasAmigos [("ana","luz"),("pato","ma"),("marcos","m"),("rita","luz")]) ~?= "luz" -- || "luz"
     ]
 
 run = runTestTT masAmigos
