d4da15a49c052237cf70be8d798544b3773aaa55
Ana Boismoreau <143358214+alboismoreau@users.noreply.github.com>
2024-04-30 23:55:01 -0300

    Add files via upload

---
+++ parciales/Parcial2.hs
@@ -0,0 +1,78 @@
+
+module Parcial2 where
+
+-- EJERCICIO 1---------------------------------------
+
+atajaronSuplentes :: [(String,String)] -> [Int] -> Int -> Int
+atajaronSuplentes _ goles total = total - sumatoria goles
+
+-- Funcion Aux Ej 1
+
+sumatoria :: (Num t) => [t] -> t
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
+
+-- EJERCICIO 2---------------------------------------
+
+equiposValidos :: [(String,String)] -> Bool
+equiposValidos l = noHayRepetidos (desentuplar l)
+
+-- Funciones Aux Ej 2
+
+desentuplar :: (Eq t) => [(t,t)] -> [t]
+desentuplar [] = []
+desentuplar ((x,y):xs) = x : y : (desentuplar xs)
+
+noHayRepetidos :: (Eq t) => [t] -> Bool
+noHayRepetidos [] = True
+noHayRepetidos (x:xs) | pertenece x xs = False
+                      | otherwise = noHayRepetidos xs
+
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece a (x:xs) | a == x = True
+                   | otherwise = pertenece a xs 
+
+-- EJERCICIO 3---------------------------------------------------
+
+porcentajeDeGoles :: String -> [(String,String)] -> [Int] -> Float
+porcentajeDeGoles arquero equipos goles = 100 * (division (indiceGoles (indiceArquero arquero equipos) goles) (sumatoria goles))
+
+-- Funciones Aux Ej 3
+
+indiceArquero :: String -> [(String,String)] -> Int
+indiceArquero _ [(x,y)] = 0
+indiceArquero a ((x,y):xs) | a == y = 0
+                           | otherwise = 1 + indiceArquero a xs
+
+indiceGoles :: Int -> [Int] -> Int
+indiceGoles n (x:xs) | n == 0 = x
+                     | otherwise = indiceGoles (n - 1) xs 
+
+division :: Int -> Int -> Float
+division a b = (fromIntegral a) / (fromIntegral b)
+
+
+-- EJERCICIO 4 -------------------------------------------------
+
+vallaMenosVencida :: [(String,String)] -> [Int] -> String
+vallaMenosVencida [(x,y)] _ = x
+vallaMenosVencida equipos goles = indiceValla (indiceMenor goles) equipos 
+
+-- Funciones Aux Ej 4
+
+indiceValla :: Int -> [(String,String)] -> String
+indiceValla _ [(x,y)] = x
+indiceValla a ((x,y):xs) | a == 0 = x
+                         | otherwise = indiceValla (a - 1) xs
+
+indiceMenor :: [Int] -> Int
+indiceMenor [x] = 0
+indiceMenor (x:xs) | x == menor (x:xs) = 0
+                   | otherwise = 1 + indiceMenor xs
+
+menor :: [Int] -> Int
+menor [x] = x
+menor (x:y:xs) | x <= y = menor (x:xs)
+               | otherwise = menor (y:xs)
+
