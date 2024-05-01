2741c7ff031a926189ddfb6e36118f4c90ff54da
Ana Boismoreau <143358214+alboismoreau@users.noreply.github.com>
2024-04-30 23:56:27 -0300

    Update Parcial2.hs

--- parciales/Parcial2.hs
+++ parciales/Parcial2.hs
@@ -1,3 +1,58 @@
+{-
+
+1) Atajaron Suplentes
+problema atajaronSuplentes (arquerosPorEquipo: seq<String X String>, goles: seq<Z>, totalGolesTorneo: Z): Z {
+	requiere: {equiposValidos(arquerosPorEquipo)
+	requiere: {|arquerosPorEquipo| = |goles|}
+	requiere: {Todos los elementos de goles son mayores o iguales a 0}
+	requiere: {La suma de todos los elementos de goles es menor o igual a totalGolesTorneo}
+	asegura: {
+	res es la cantidad de goles recibidos en el torneo por arqueros que no son titulares en sus equipos.
+	}
+}
+
+
+2) Equipos Válidos
+problema equiposValidos (arquerosPorEquipo: seq<String X String>): Bool {
+	requiere: {True}
+	asegura: {
+	(res = True) <=> arquerosPorEquipo no contiene nombres de clubes repetidos, ni arqueros repetidos, ni jugadores con nombre del club
+	}
+}
+
+
+3) Porcentaje de goles
+problema porcentajeDeGoles (arquero: String, arquerosPorEquipo: seq<String X String>, goles: seq<Z>): R {
+	requiere: {La segunda componente de algún elemento de arquerosPorEquipo es arquero}
+	requiere: {equiposValidos(arquerosPorEquipo)}
+	requiere: {|arquerosPorEquipo| = |goles|}
+	requiere: {Todos los elementos de goles son mayores o iguales a 0}
+	requiere: {Hay al menos un elemento de goles mayores estricto a 0}
+	asegura: {
+	res es el porcentaje de goles que recibió arquero sobre el total de goles recibidos por arqueros titulares
+	}
+}
+
+Para resolver este ejercicio pueden utilizar la siguiente función que devuelve como float la división entre dos
+numeros de tipo Int.
+
+division :: Int -> Int -> Float
+division a b = fromIntegral a / fromIntegral b
+
+
+4) Valla Menos Vencida
+problema vallaMenosVencida (arquerosPorEquipo: seq<String X String>, goles: seq<Z>): String {
+	requiere: {equiposValidos(arquerosPorEquipo)}
+	requiere: {|arquerosPorEquipo| = |goles|}
+	requiere: {Todos los elementos de goles son mayores o iguales a 0}
+	requiere: {|goles| > 0}
+	asegura: {
+	res es alguno de los arqueros de arquerosPorEquipo que menor goles recibió de acuerdo a goles
+	}
+}
+
+-}
+
 
 module Parcial2 where
 
